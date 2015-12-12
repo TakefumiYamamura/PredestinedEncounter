18cc3c7217c7bd517beb6ab184cf555831084cef
TakeumiYamamura <yama1223xxx@gmail.com>
2015-12-12 20:14:04 +0900

    add make shadow function

--- PredestinedEncounter/LoginViewController.swift
+++ PredestinedEncounter/LoginViewController.swift
@@ -11,21 +11,36 @@ import Parse
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var loginView: UIImageView!
+    @IBOutlet weak var topTitle: UILabel!
+    @IBOutlet weak var subDiscription: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let coverView = UIView()
         coverView.frame = CGRectMake(0, 0, view.frame.width, view.frame.height)
         coverView.backgroundColor = UIColor.blackColor()
-        coverView.alpha = 0.2
+        coverView.alpha = 0.3
         loginView.addSubview(coverView)
-
+        
+        topTitle = makeShadow(topTitle)
+        subDiscription = makeShadow(subDiscription)
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func makeShadow(label: UILabel) -> UILabel {
+        label.layer.shadowRadius = 0
+        label.layer.shadowOffset = CGSize(width: 3, height: 3)
+        label.layer.shadowColor  = UIColor.blackColor().CGColor
+        label.layer.shadowOpacity = 0.8
+        label.layer.shadowRadius = 5
+    
+        return label
     }
     
 
