3a83febc66cb71cddaec9f7e8dbed2035c908260
TakeumiYamamura <yama1223xxx@gmail.com>
2015-12-13 04:48:50 +0900

    add message list table view controller

---
+++ PredestinedEncounter/MessageListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageListTableViewCell.swift
+//  PredestinedEncounter
+//
+//  Created by TakefumiYamamura on 2015/12/13.
+//  Copyright © 2015年 ids. All rights reserved.
+//
+
+import UIKit
+
+class MessageListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
